c135668540ab9149178021403041b6af8884fa5c
Anton <a.tashchilin@bk.ru>
2024-01-04 22:02:07 +0400

    add findByUsername

--- src/main/java/com/service/chatservice/repositories/UserRepo.java
+++ src/main/java/com/service/chatservice/repositories/UserRepo.java
@@ -1,5 +1,7 @@
 package com.service.chatservice.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.service.chatservice.domain.user.User;
 @Repository
 public interface UserRepo extends JpaRepository<User,Long>{
     
+    Optional<User> findByUsername(String username);
+
 }
