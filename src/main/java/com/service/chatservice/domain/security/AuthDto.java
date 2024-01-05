3b1294e45709a4687eee29e5af3b00d882f3a97f
Anton <a.tashchilin@bk.ru>
2024-01-05 20:24:42 +0400

    add builder

--- src/main/java/com/service/chatservice/domain/security/AuthDto.java
+++ src/main/java/com/service/chatservice/domain/security/AuthDto.java
@@ -3,12 +3,14 @@ package com.service.chatservice.domain.security;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuthDto {
     
     private String username;
