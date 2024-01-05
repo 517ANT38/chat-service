28c18795a0f9ee4fad66984f0374685a9909bc3a
Anton <a.tashchilin@bk.ru>
2024-01-05 08:06:19 +0400

    chnage method mapper chat

--- src/main/java/com/service/chatservice/domain/chat/mapper/MapperChat.java
+++ src/main/java/com/service/chatservice/domain/chat/mapper/MapperChat.java
@@ -6,12 +6,13 @@ import org.mapstruct.ReportingPolicy;
 
 import com.service.chatservice.domain.chat.Chat;
 import com.service.chatservice.domain.chat.dto.ChatDto;
+import com.service.chatservice.domain.chat.dto.NewChatDto;
 
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MapperChat {
     
     @InheritInverseConfiguration
-    Chat map(ChatDto dto);
+    Chat map(NewChatDto dto);
     
     ChatDto map(Chat chat);
 }
