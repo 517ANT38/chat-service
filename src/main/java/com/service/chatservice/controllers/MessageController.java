0594db340ff4e07defc443b9652c55b122cd8251
Anton <a.tashchilin@bk.ru>
2024-01-05 15:50:57 +0400

    add methods into message controller

--- src/main/java/com/service/chatservice/controllers/MessageController.java
+++ src/main/java/com/service/chatservice/controllers/MessageController.java
@@ -1,13 +1,24 @@
 package com.service.chatservice.controllers;
 
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.service.chatservice.app.NotFoundException;
+import com.service.chatservice.domain.message.Message;
+import com.service.chatservice.domain.message.MessageStatus;
+import com.service.chatservice.domain.message.dto.MessageDto;
+import com.service.chatservice.domain.message.dto.NewMessageDto;
 import com.service.chatservice.domain.message.mapper.MapperMessage;
 import com.service.chatservice.services.MessageService;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -16,4 +27,22 @@ import lombok.RequiredArgsConstructor;
 public class MessageController {
     private final MessageService service;
     private final MapperMessage mapperMessage;
+
+    @PostMapping("/new")
+    public ResponseEntity<MessageDto> save(@RequestBody NewMessageDto message){
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapperMessage
+            .map(service.save(mapperMessage.map(message))));
+    }    
+
+    @GetMapping("/{id}")
+    public Message findById(@PathVariable("id") long id){
+        return service.findById(id);
+               
+    }
+    
+    @GetMapping("/chat/{chatId}")
+    public Set<Message> findByChatId(@PathVariable("chatId") long chatId){
+        return service.findByChatId(chatId); 
+    }
+
 }
--- src/main/java/com/service/chatservice/services/MessageService.java
+++ src/main/java/com/service/chatservice/services/MessageService.java
@@ -1,10 +1,13 @@
 package com.service.chatservice.services;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
 import com.service.chatservice.app.NotFoundException;
 import com.service.chatservice.domain.message.Message;
 import com.service.chatservice.domain.message.MessageStatus;
+import com.service.chatservice.repositories.ChatRepo;
 import com.service.chatservice.repositories.MessageRepo;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class MessageService {
     
     private final MessageRepo messageRepo;
+    private final ChatRepo chatRepo;
 
     public Message save(Message message){
         return messageRepo.save(message);
@@ -24,5 +28,17 @@ public class MessageService {
             .orElseThrow(() -> new NotFoundException("Message not found with id=" + id));
         m.setStatus(MessageStatus.DELIVERED);
         messageRepo.save(m);
+    }
+
+    public Message findById(long id){
+        return messageRepo.findById(id)
+            .orElseThrow(() -> new NotFoundException("Message not found with id=" + id));
+               
+    }
+
+    public Set<Message> findByChatId(long chatId){
+        var chat = chatRepo.findById(chatId)
+            .orElseThrow(() -> new NotFoundException("Message not found with id=" + chatId));
+        return chat.getMessages(); 
     }
 }
