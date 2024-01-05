ea4f60f0ca8ebe25178bbc4ae38289acc9444f1e
Anton <a.tashchilin@bk.ru>
2024-01-05 16:59:28 +0400

    add creater admin

--- src/main/java/com/service/chatservice/services/UserService.java
+++ src/main/java/com/service/chatservice/services/UserService.java
@@ -2,6 +2,7 @@ package com.service.chatservice.services;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.service.chatservice.app.ApiException;
@@ -9,6 +10,8 @@ import com.service.chatservice.app.NotFoundException;
 import com.service.chatservice.domain.user.User;
 import com.service.chatservice.repositories.UserRepo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,14 +19,33 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
     
     private final UserRepo userRepo;
+    private final PasswordEncoder encoder;
 
+    @PostConstruct
+    @Transactional
+    void defaultAdmin(){
+        if(!userRepo.existsByUsername("admin")){
+            var user = userRepo.save(User.builder()
+                .username("admin")
+                .name("admin")
+                .password(encoder.encode("admin"))
+                .role("ROLE_ADMIN")
+                .enabled(true)
+                .build());
+                System.out.println(user);
+        }
+    }
+
+    @Transactional
     public User newUser(User user){
         
         if(userRepo.existsByUsername(user.getUsername())){
             throw new ApiException("A user with this username" + user.getUsername() + "exists");
         }
             
-        return userRepo.save(user);
+        return userRepo.save(user.toBuilder()
+            .password(encoder.encode(user.getPassword()))
+            .build());
     }
 
     public User findByUsername(String  username){
