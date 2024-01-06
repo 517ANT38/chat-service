f653daa05ae2db4e964becdc9f36ce8783ed7ead
Anton <a.tashchilin@bk.ru>
2024-01-06 07:34:35 +0400

    add builder

--- src/main/java/com/service/chatservice/domain/chat/dto/NewChatDto.java
+++ src/main/java/com/service/chatservice/domain/chat/dto/NewChatDto.java
@@ -1,8 +1,10 @@
 package com.service.chatservice.domain.chat.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class NewChatDto {
     private String name;
     private boolean privateChat;
